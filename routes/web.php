e89c2d969a95e8e6432073dd47ac03cef1713e06
Marco Woldering <marco.woldering@trimm.nl>
2019-02-05 16:07:38 +0100

    Added functionality to upload images

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,52 @@ $router->post('/contents', function (Request $request) use ($router) {
 
     $sha = $file['sha'];
 
+    // Check if images are added.
+    $images_to_upload = [];
+    if ($request->session()->has('images')) {
+        foreach ($request->session()->get('images') as $image) {
+            if (strpos($contents, $image) !== false) {
+                array_push($images_to_upload, $image);
+            }
+        }
+    }
+
+    // Commit images
+    if (!empty($images_to_upload)) {
+        // Get latest commit SHA
+        $commits = $client->api('repo')->commits()->all(
+            env('GITHUB_USER'),
+            env('GITHUB_REPO'),
+            array('sha' => 'master')
+        );
+        $latest_sha = $commits[0]['sha'];
+
+        // Create branch
+        $branch = 'heads/images' . time();
+        $reference = 'refs/' . $branch;
+        $client->api('gitData')->references()->create(
+            env('GITHUB_USER'),
+            env('GITHUB_REPO'),
+            ['ref' => $reference, 'sha' => $latest_sha]
+        );
+
+        // Commit images to branch
+        foreach ($images_to_upload as $image) {
+           $image_path = 'docs/images/' . $image;
+           $data = file_get_contents(storage_path('app/uploads/' . $image));
+           $client->api('repo')->contents()->create(
+               env('GITHUB_USER'),
+               env('GITHUB_REPO'),
+               $image_path,
+               $data,
+               'Added image via ToastUI!',
+               $reference,
+               ['name' => $username, 'email' => $provider->getResourceOwner($request->session()->get('oauth_token'))->getEmail() ?? $username . '@unknown.uk']
+           );
+        }
+    }
+
+    // Commit MD
     $client->api('repo')->contents()->update(
         env('GITHUB_USER'),
         env('GITHUB_REPO'),
@@ -49,6 +95,32 @@ $router->post('/contents', function (Request $request) use ($router) {
         'master',
         ['name' => $username, 'email' => $provider->getResourceOwner($request->session()->get('oauth_token'))->getEmail() ?? $username . '@unknown.uk']
     );
+
+    if (!empty($images_to_upload)) {
+        // Merge image branch into master
+        $client->api('repo')->merge(
+            env('GITHUB_USER'),
+            env('GITHUB_REPO'),
+            'master',
+            $branch,
+            'merge ' . $branch . ' into master.'
+        );
+
+        // Delete image branch
+        $client->api('gitData')->references()->remove(
+            env('GITHUB_USER'),
+            env('GITHUB_REPO'),
+            $branch
+        );
+
+        // Delete images
+        foreach ($request->session()->get('images') as $image) {
+            unlink(storage_path('app/uploads/' . $image));
+        }
+
+        // Clear session images
+        $request->session()->remove('images');
+    }
 });
 
 
@@ -117,7 +189,15 @@ $router->get('/callback', function (Request $request) use ($router) {
     }
 });
 
-$router->get('/images/{file:.+}', function ($file) {
+$router->get('/images/{file:.+}', function ($file, Request $request) {
+    if ($request->session()->has('images')) {
+        foreach($request->session()->get('images') as $image) {
+            if ($file == $image) {
+                return file_get_contents(storage_path('app/uploads/' . $image));
+            }
+        }
+    }
+
     $client = new Client();
     $client->authenticate(env('GITHUB_TOKEN'), null,Client::AUTH_URL_TOKEN);
 
@@ -126,4 +206,32 @@ $router->get('/images/{file:.+}', function ($file) {
     );
 
     return file_get_contents($file['download_url']);
+});
+
+$router->post('/upload', function (Request $request) {
+    // Check if upload is an image
+    $image_parts = explode(";base64,", $request->input('content'));
+    $img = $image_parts[1];
+    if(!imagecreatefromstring(base64_decode($img))) {
+        return "Something went wrong uploading the image";
+    }
+
+    // If upload folder doesn't exists create one.
+    if(!is_dir(storage_path('app/uploads'))) {
+        mkdir(storage_path('app/uploads'));
+    }
+
+    // Generate unique filename
+    $ext = pathinfo($request->input('name'), PATHINFO_EXTENSION);
+    $unique_filename = uniqid() . '.' . $ext;
+
+    // Save file in upload folder
+    file_put_contents(storage_path('app/uploads/' . $unique_filename),  base64_decode($img));
+
+    // Set image name in Session
+    $image_array = $request->session()->has('images') ? $request->session()->get('images') : [];
+    array_push($image_array, $unique_filename);
+    $request->session()->put('images', $image_array);
+
+    return "./images/" . $unique_filename;
 });
