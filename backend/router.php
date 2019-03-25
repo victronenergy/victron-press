3498e255237302b23b96b4d95107443dd7f59fec
Daniël van de Giessen <daniel@dvdgiessen.nl>
2019-03-25 13:51:28 +0100

    Added 5-minute cache in router script

--- backend/router.php
+++ backend/router.php
@@ -1,6 +1,7 @@
 <?php
 // Router for local use with PHP's built-in webserver
 if (file_exists($_SERVER['DOCUMENT_ROOT'] . preg_replace('/\?.*$/', '', $_SERVER['REQUEST_URI']))) {
+    header('Expires: ' . gmdate('D, d M Y H:i:s', time() + 300) . ' GMT');
     return false;
 } else {
     require $_SERVER['DOCUMENT_ROOT'] . '/index.php';
